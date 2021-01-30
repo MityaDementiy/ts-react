a325009f33c04a4e357580eee8ffea27dab46a6b
MityaDementiy <d.dementiy@gmail.com>
2021-01-30 18:26:37 +0200

    Add handlers and state to componenents

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
 
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (title: string) => {
+    console.log('new todo', title);
+  };
+
   return (
     <>
       <Navbar />
       <div className="container">
-        <TodoForm />
+        <TodoForm onAdd={handleAdd} />
       </div>
     </>
   );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
-export const TodoForm: React.FC = () => {
+interface TodoFormProps {
+  onAdd(title: string): void,
+};
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+  const [title, setTitle] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      console.log(title);
+      setTitle('');
+    }
+  };
+
   return (
     <div className="input-field pd-top-20">
-      <input type="text" id="title" placeholder="Введите название дела"/>
+      <input 
+        type="text" 
+        id="title" 
+        placeholder="Введите название дела"
+        value={title}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
       <label htmlFor="title" className="active pd-top-20">Введите название дела</label>
     </div>
   );
